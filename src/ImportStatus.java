7159decb3706e48d98f18e08a111803e98845d82
MileneBedouhene <milenebedouhene@gmail.com>
2024-03-22 16:54:06 +0100

    main

--- src/ImportStatus.java
+++
@@ -1,9 +0,0 @@
-
-public class ImportStatus {
-String ImportName;
-int ImportStatus;
-ImportStatus(String ImportName,int ImportStatus){
-	this.ImportName=ImportName;
-	this.ImportStatus=ImportStatus;
-}
-}
