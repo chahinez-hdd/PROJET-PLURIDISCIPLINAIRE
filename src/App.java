2da67704fcc5aa58719b6b8ef2c946ba044f5cc2
chahinez-hdd <chahinez.haddad2005@gmail.com>
2024-03-28 03:27:44 +0100

    metric-collaboration-des-classes

--- src/App.java
+++ src/App.java
@@ -1,18 +1,45 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        String FilePtah="chemin vers le fichier";
-        File file = new File(FilePtah);
-        Scanner sc = new Scanner(file);
-
-        int cpt1=0,cpt2=0;
-        String word;
-        while(sc.hasNext()){
-            word = sc.next();
-            if(word=="private"||word=="protected")  cpt1++;
-            if(word=="int"||word=="double"||word=="bool"||word=="private"||word=="protected"||word=="private"||word=="protected"){}
+    public class App  {
+    
+    
+        public static void nombreInstancesClasses (File repertory, File file) {
+            
+             for (File fichier : repertory.listFiles()) {
+                try (Scanner scFile = new Scanner(file); Scanner scRepertory = new Scanner(fichier)) {
+               
+                    int cpt = 0;
+                    if (fichier.isFile()) {
+                        scFile.nextLine();
+                        String nameClass = fichier.getName();
+                        nameClass = nameClass.replace(".java", ""); 
+                        while (scFile.hasNextLine()) {
+                            if (scFile.nextLine().contains(nameClass)) cpt++;
+                        }
+                        System.out.println("La classe: " + nameClass + " a été instanciée: " + cpt + " fois dans votre fichier " + file.getName());
+        
+                    } else if (fichier.isDirectory()) {
+                        System.out.println("Répertoire: " + fichier.getName());
+                        nombreInstancesClasses(fichier, file); 
+                    }
+                
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+
+    public static void main(String[] args) throws  FileNotFoundException {
+        String FilePtah="C:\\Users\\user\\Documents\\vscode\\JAVA\\testsMetrics\\SecurityMetric\\src\\src2\\test2.java";
+        File file = new File(FilePtah);
+        
+        
+        String RepertoryPath ="C:\\Users\\user\\Documents\\vscode\\JAVA\\testsMetrics\\SecurityMetric\\src\\src2";
+        File repertory = new File(RepertoryPath);
+        nombreInstancesClasses(repertory,file);
+        
     }
 }
---
+++ src/src2/test2.java
@@ -0,0 +1,9 @@
+package src2;
+
+public class test2 {
+    test1 hi = new test1();
+    
+    test1 hio = new test1();
+    
+    test1 hi2 = new test1();
+}
