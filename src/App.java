c491495a3ad19e9687e846dd54321eb2ef32439d
chahinez-hdd <chahinez.haddad2005@gmail.com>
2024-03-28 01:09:23 +0100

    version num1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-      
+        String FilePtah="chemin vers le fichier";
+        File file = new File(FilePtah);
+        Scanner sc = new Scanner(file);
+
+        int cpt1=0,cpt2=0;
+        String word;
+        while(sc.hasNext()){
+            word = sc.next();
+            if(word=="private"||word=="protected")  cpt1++;
+            if(word=="int"||word=="double"||word=="bool"||word=="private"||word=="protected"||word=="private"||word=="protected"){}
+        }
     }
 }
