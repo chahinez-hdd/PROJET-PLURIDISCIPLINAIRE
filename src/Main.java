47ec660b08e0ff9d2ebd546b8b81d5e87cfbf4e1
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-03-31 15:50:56 +0200

    deleted old files

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Main {
-	public static void main(String args[]) {
-		String path = "C:\\Users\\DELL\\eclipse-workspace\\Metrics";
-		ArrayList<Package>list = Package.NumberLineJavaProject(path);
-		Package.PrintFileNumberLine(list);
-	}
-}
